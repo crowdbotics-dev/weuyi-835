6cb9ed44e3411c10fd6b6f9d4be9cc823ba0d9b3
Crowdbotics <engineering@crowdbotics.com>
2024-09-27 08:35:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weuyi-835",
+  url: "https://weuyi-835.botics.co",
+  api: "https://weuyi-835.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
